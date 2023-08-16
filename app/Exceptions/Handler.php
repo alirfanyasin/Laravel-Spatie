c1c071d17a5ec9092f2492a511750474e453fe56
alirfanyasin <risayunita21@gmail.com>
2023-08-16 17:33:46 +0700

    feat: exception error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,16 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+    public function render($request, Throwable $error)
+    {
+
+        if ($error instanceof UnauthorizedException) {
+            return response()->view('errors.forbidden', ['exceptions' => $error->getMessage()], 403);
+        }
+
+        return parent::render($request, $error);
     }
 }
